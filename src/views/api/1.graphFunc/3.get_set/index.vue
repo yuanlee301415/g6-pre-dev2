c1c9b4c07a7ad757f52eec3403e05671d233ca11
yuanlee <yuanlee@163.com>
2022-04-22 09:07:12 +0800

    Done: 实例方法》获取与设置

---
+++ src/views/api/1.graphFunc/3.get_set/index.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <div id="container" style="width: 800px;height: 600px;border: 1px solid green;"></div>
+    <dl>
+      <dt>Get</dt>
+      <dd>
+        <button @click="handleGetContainer">GetContainer</button>
+        <button @click="handleGetGroup">GetGroup</button>
+        <button @click="handleGetMaxZoom">GetMaxZoom</button>
+        <button @click="handleGetMinZoom">GetMinZoom</button>
+        <button @click="handleGetWidth">GetWidth</button>
+        <button @click="handleGetHeight">GetHeight</button>
+      </dd>
+    </dl>
+    <dl>
+      <dt>Set</dt>
+      <dd>
+
+      </dd>
+    </dl>
+  </div>
+</template>
+
+<script>
+import G6 from "@antv/g6";
+import data from '../../data.json'
+
+let graph
+
+export default {
+  name: "GetSet",
+  mounted() {
+    this.init()
+  },
+  methods: {
+    init() {
+      const container = document.getElementById('container');
+      const { scrollWidth, scrollHeight } = container
+
+      graph = new G6.Graph({
+        container,
+        width: scrollWidth,
+        height: scrollHeight,
+        fitView: true,
+        layout: {
+          type: 'random'
+        }
+      });
+      graph.data(data)
+      graph.render()
+      console.log('graph:', graph)
+    },
+    handleGetContainer() {
+      const container = graph.getContainer()
+      console.log('container:', container)
+    },
+    handleGetGroup() {
+      const group = graph.getGroup()
+      console.log('group:', group)
+    },
+    handleGetMaxZoom() {
+      const maxZoom = graph.getMaxZoom()
+      console.log('maxZoom:', maxZoom)
+    },
+    handleGetMinZoom() {
+      const minZoom = graph.getMinZoom()
+      console.log('minZoom:', minZoom)
+    },
+    handleGetWidth() {
+      const width = graph.getWidth()
+      console.log('width:', width)
+    },
+    handleGetHeight() {
+      const height = graph.getHeight()
+      console.log('height:', height)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
