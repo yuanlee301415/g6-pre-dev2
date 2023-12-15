70275c4acb493e173b299fd8b9562ee7e77f960c
yuanlee <yuanlee@163.com>
2023-12-15 13:19:39 +0800

    Done: 高亮节点及关系

--- src/views/topology/Topology.js
+++ src/views/topology/Topology.js
@@ -28,19 +28,77 @@ function init(G6, options) {
         layout: {
             type: 'gForce'
         },
+        defaultNode: {
+            style: {
+                fill: '#10b981',
+                lineWidth: 0
+            },
+            labelCfg: {
+                position: 'bottom',
+                style: {
+                    fill: '#333',
+                    fontSize: 7
+                }
+            }
+        },
         defaultEdge: {
             type: 'quadratic',
             style: {
+                stroke: '#10b981',
+                lineWidth: 1,
                 endArrow: {
                     path: G6.Arrow.vee(5, 6, 3),
                     d: 3
                 }
+            },
+            labelCfg: {
+                style: {
+                    fill: '#999',
+                    fontSize: 6
+                },
             }
         },
         modes: {
-            default: ['drag-canvas', 'zoom-canvas', 'click-select', 'drag-node']
+            default: ['drag-canvas', 'zoom-canvas', 'drag-node',
+                {
+                    /**
+                     * 当鼠标移到某节点时，突出显示该节点以及与其直接关联的节点和连线；
+                     */
+                    type: 'activate-relations',
+                    trigger: 'click'
+                }
+            ]
         },
         plugins: [menu],
+        nodeStateStyles: {
+            active: {
+                fill: '#10b981',
+                lineWidth: 0,
+                'text-shape': {
+                    fill: '#10b981'
+                }
+            },
+            inactive: {
+                opacity: 0.8,
+                'text-shape': {
+                    opacity: 0.8
+                }
+            }
+        },
+        edgeStateStyles: {
+            active: {
+                stroke: '#10b981',
+                'text-shape': {
+                    fill: '#10b981'
+                }
+            },
+            inactive: {
+                opacity: 0.8,
+                'text-shape': {
+                    opacity: 0.8
+                }
+            }
+        }
     })
     return graph
 }
